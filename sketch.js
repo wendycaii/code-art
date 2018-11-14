ef9b47b95a8c86bed49b64311199e3db65beb62a
Wendy Cai <wanyi.cai@sjsu.edu>
2018-11-13 17:35:44 -0800

    updated

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 400);
+  textSize(64)
+  fill(255, 0, 0);
+}
+
+function draw() {
+  background(220);
+  text(key, 60, 80);
+  text(key, 100,80);
+  text(key, 140,80);
+  text(key, 180,80);
+}
